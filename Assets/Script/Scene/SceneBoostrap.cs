c9717afc24992d3210feb530d6f1fab79276ab5a
ATUM91 <darkholic290@gmail.com>
2026-05-04 21:58:34 +0900

    feat Bootstrap 씬, 스크립트 추가

---
+++ Assets/Script/Scene/SceneBoostrap.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneBoostrap : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene("Scene Title");   
+    }
+}
--- Assets/Script/Scene/SceneLoading.cs
+++ Assets/Script/Scene/SceneLoading.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 using System.Collections;
 
 // 로딩 연출
@@ -8,7 +10,11 @@ using System.Collections;
 public class SceneLoading : MonoBehaviour
 {
     private static string targetScene;  // 이동할 씬 이름
-    private float loadingTime = 2f;     // 로딩 연출 대기 시간
+    
+    [SerializeField] private Slider loadingGauge;   // 인스펙터에 로딩바 넣기
+    [SerializeField] private TMP_Text loadingText;      // 인스펙터에 퍼센트 텍스트 넣기
+
+    private float loadingTime = 1f;     // 로딩 연출 대기 시간 / 로딩시간이 상당히 짧아서 대기 1초 넣음
 
     // 외부에서 호출
     public static void LoadTo(string sceneName)
@@ -21,14 +27,36 @@ public class SceneLoading : MonoBehaviour
 
     void Start()
     {
+        loadingGauge.value = 0f; // 로딩바 초기화
+        loadingText.text = "0%";
         StartCoroutine(Load());
     }
 
     IEnumerator Load()
     {
-        // 로딩 연출중 딜레이 
-        yield return new WaitForSeconds(loadingTime);
-        // 목표한 최종 씬 이동
-        SceneManager.LoadScene(targetScene);
+        AsyncOperation ao = SceneManager.LoadSceneAsync(targetScene);
+        ao.allowSceneActivation = false;
+
+        float current = 0f; // 로딩 게이지 초기값
+
+        while (!ao.isDone)
+        {
+            // 1. 로딩 게이지 세팅
+            float target = Mathf.Clamp01(ao.progress / 0.9f);
+            current = Mathf.MoveTowards(current, target, Time.deltaTime);
+            loadingGauge.value = current;
+
+            // 2. 로딩 퍼센트 세팅
+            int pecent = Mathf.RoundToInt(current * 100f);
+            loadingText.text = pecent + "%";
+
+            if (current >= 1f)
+            {
+                // 로딩 연출중 딜레이
+                yield return new WaitForSeconds(loadingTime);
+                ao.allowSceneActivation = true;
+            }
+            yield return null;
+        }
     }
 }
